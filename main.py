6eb724506f9c103d2d6bcc239a297d4a7a55802b
Santosh KV <theaimonk24@gmail.com>
2024-01-04 20:44:36 -0600

    Full update

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from ml_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPip
 from ml_project.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from ml_project.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from ml_project.pipeline.stage_04_model_trainer import ModelTrainerPipeline
-
+from ml_project.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 def run_pipeline_stage(stage_name, pipeline_class):
     """
@@ -17,7 +17,7 @@ def run_pipeline_stage(stage_name, pipeline_class):
         logger.info(">>>>> %s Started <<<<<", stage_name)
         pipeline_obj = pipeline_class()
         pipeline_obj.main()
-        logger.info(">>>>> %s Completed <<<<<", stage_name)
+        logger.info(">>>>> %s Completed <<<<<\nx==========x", stage_name)
     except Exception as e:
         logger.exception(e)
         raise e
@@ -27,3 +27,4 @@ run_pipeline_stage('Data Ingestion Stage', DataIngestionTrainingPipeline)
 run_pipeline_stage('Data Validation Stage', DataValidationTrainingPipeline)
 run_pipeline_stage('Data Transformation Stage', DataTransformationTrainingPipeline)
 run_pipeline_stage('Model Training Stage', ModelTrainerPipeline)
+run_pipeline_stage('Evaluation of Model', ModelEvaluationTrainingPipeline)
--- src/ml_project/config/configuration.py
+++ src/ml_project/config/configuration.py
@@ -3,8 +3,8 @@ from ml_project.utils.common import read_yaml, create_directories
 from ml_project.entity.config_entity import (DataIngestionConfig,
                                             DataValidationConfig,
                                             DataTransformationConfig,
-                                            ModelTrainerConfig)
-                                            # ModelEvaluationConfig)
+                                            ModelTrainerConfig,
+                                            ModelEvaluationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -20,7 +20,6 @@ class ConfigurationManager:
         create_directories([self.config.artifacts_root])
 
 
-    
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
 
@@ -52,7 +51,6 @@ class ConfigurationManager:
         return data_validation_config
     
 
-
     def get_data_transformation_config(self) -> DataTransformationConfig:
         config = self.config.data_transformation
 
@@ -65,7 +63,6 @@ class ConfigurationManager:
 
         return data_transformation_config
     
-
 
     def get_model_trainer_config(self) -> ModelTrainerConfig:
         config = self.config.model_trainer
@@ -82,29 +79,24 @@ class ConfigurationManager:
             alpha = params.alpha,
             l1_ratio = params.l1_ratio,
             target_column = schema.name
-            
         )
-
         return model_trainer_config
     
 
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config = self.config.model_evaluation
+        params = self.params.ElasticNet
+        schema = self.schema.TARGET_COLUMN
 
-    # def get_model_evaluation_config(self) -> ModelEvaluationConfig:
-    #     config = self.config.model_evaluation
-    #     params = self.params.ElasticNet
-    #     schema =  self.schema.TARGET_COLUMN
+        create_directories([config.root_dir])
 
-    #     create_directories([config.root_dir])
-
-    #     model_evaluation_config = ModelEvaluationConfig(
-    #         root_dir=config.root_dir,
-    #         test_data_path=config.test_data_path,
-    #         model_path = config.model_path,
-    #         all_params=params,
-    #         metric_file_name = config.metric_file_name,
-    #         target_column = schema.name,
-    #         mlflow_uri="https://dagshub.com/entbappy/End-to-end-Machine-Learning-Project-with-MLflow.mlflow",
-           
-    #     )
-
-    #     return model_evaluation_config
+        model_evaluation_config = ModelEvaluationConfig(
+            root_dir=config.root_dir,
+            test_data_path=config.test_data_path,
+            model_path = config.model_path,
+            all_params=params,
+            metric_file_name = config.metric_file_name,
+            target_column = schema.name,
+            mlflow_uri="https://dagshub.com/gyannetics/mlops-end-to-end.mlflow",   
+        )
+        return model_evaluation_config
